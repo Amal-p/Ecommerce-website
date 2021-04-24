9e61ca099fb4425b943f270cccf5dfcd5de85cd8
Amal-p <amalpvatayam67@gmail.com>
2021-04-24 15:59:50 +0530

    User page and admin page setup by original datafrom Database

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,10 +1,17 @@
 var db = require("../config/connection")
+var collection = require('../config/collection')
 
 module.exports = {
     addProduct:(product,callback)=>{
         db.get().collection('product').insert(product).then((data) => {
             // console.log(data.ops[0]._id)
             callback(data.ops[0]._id)
+        })
+    },
+    getAllProduct:()=>{
+        return new Promise((resolve, reject)=>{
+            let products = db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     }
 }
